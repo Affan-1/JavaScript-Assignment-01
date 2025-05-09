6defe577d0de0bcb39d416f12f9d3a410adc211c
Muhammad Affan <167810592+Affan-1@users.noreply.github.com>
2025-05-09 11:24:15 +0500

    Add files via upload

---
+++ assignnment.js
@@ -0,0 +1,66 @@
+// Assignment 01
+
+// Question 01
+alert("Welcome to our website, we are glad to be here");
+
+// Question 02
+alert("Error! Please enter a valid password");
+
+// Question 03
+alert("Welcome to JS Land...\n Happy Coding!");
+
+// Question 04
+alert("Welcome to JS Land");
+
+// Question 05
+alert("Happy Coding!");
+
+// Question 06
+alert("Hello... I can run JS through my web browser's console");
+
+console.log("alert(Hello... I can run JS through my web browser's console)");
+
+// Question 07 
+var username;
+var myName = "Muhammad Affan";
+var titled_message;
+titled_message = "Hello World";
+
+alert("Hi Muhammad Affan, Welcome to Hello world");
+
+// Question 08
+var Name = "Muhammad Affan";
+var age = 19;
+var institute = "SMIT";
+var course = "Web Development course";
+
+console.log(Name);
+console.log(age);
+console.log(institute);
+console.log(course);
+
+var Myage = 19;
+console.log("I am 14 years old");
+
+// Question 09
+var myBirthyear = 2008;
+console.log("My birth year is 2008");
+console.log( "Data type of my declared variable is " + typeof myBirthyear);
+
+// Question 10
+var childernsName = 2;
+var partnersName = "ABC";
+var geographicalLocation = "US";
+var jobTittle = "Software Developer";
+
+console.log("You will be a" +" " + (jobTittle) +" " + "in" +" "  +  (geographicalLocation) +" " + "and married to" +" " + (partnersName) +" " + "with" +" " + (childernsName) +" " + "kids");
+
+// Question 10
+var email = "headeralishah@gmail.com";
+console.log("My Email address is headeralishah@gmail.com");
+
+alert("My Email address is headeralishah@gmail.com");
+
+// Question 11
+var book = "A Smarter way to learn JavaScript";
+alert("I am trying to learn from the book 'A Smarter way to learn JavaScript'");
